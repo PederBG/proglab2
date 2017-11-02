d36b7060b48ea78d29eb1553323a809ef2c73dd0
Andreas <andreas.haukeland.95@gmail.com>
2017-11-02 16:20:16 +0100

    Auto stash before merge of "master" and "origin/master"

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from BBCON import BBCON
 from Arbitrator import Arbitrator
 from Sensob import LookAhead
+from Motob import Motob
+from Behaviors import Approach, BackOff
 
 bbcon = BBCON()
-bbcon.add_behavior()
-bbcon.add_behavior()
+
+bbcon.add_behavior(Approach())
+bbcon.add_behavior(BackOff())
+
 bbcon.add_sensObj(LookAhead())
+
 bbcon.add_arbitrator(Arbitrator())
-bbcon.add_mot_obj()
+bbcon.add_mot_obj(Motob())
 
 while(True):
     bbcon.run_one_timestep()
+    motor_recommendation = bbcon.Arbitrator.chooseBest()
+    bbcon.Mot_Objs.apply_motor_recommendation()
