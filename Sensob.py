e2a67e4e2d28085d10b5a5cd1ece90d9ea60f219
Andreas <andreas.haukeland.95@gmail.com>
2017-11-02 14:55:05 +0100

    Add Sensob

--- Sensob.py
+++ Sensob.py
@@ -1,13 +1,23 @@
-class Sensob:
+from ultrasonic import Ultrasonic
 
-    def __init__(self, name, sensors = [], value = 0):
-        self.name = name
-        self.sensors = sensors
-        self.value = value
+class Sensob(object):
+    def __init__(self):
+        pass
 
     def update(self):
-        #Temporary code for receiving values from sensors
-        for sensor in self.sensors:
-            tempValue = sensor.get_value()
+        pass
 
-            #Najs
+    def get_value(self):
+        pass
+
+class LookAhead(Sensob):
+    def __init__(self):
+        self.sensor = Ultrasonic()
+        self.value = 0
+
+    def update(self):
+        self.sensor.update()
+        self.value = self.sensor.get_value()
+
+    def get_value(self):
+        return self.value
