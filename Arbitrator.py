7dcf421f4ed7b2aea6bb61e43829eaf7a4ad0e4a
Larsern <laberge@stud.ntnu.no>
2017-11-02 15:54:40 +0100

    Arbitrator

--- Arbitrator.py
+++ Arbitrator.py
@@ -1,13 +1,17 @@
 
 ## The motor object (motob) manifests an interface between a behavior and one or more motors (a.k.a. actuators)
 
-import BBCON
+from Proglab2.Øving6.Proglab6.BBCON import BBCON
+import operator
 
 class arbitrator():
 
     def __init__(self):
         self.behaviors = {"Approche": 0, "Back Off": 0}
         self.BBCONsActive_behaviors = BBCON.Active_behaviors
+
+
+
 
     def uppdate(self):
         for behaveObj in self.BBCONsActive_behaviors:
