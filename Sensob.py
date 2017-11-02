a90e85f16f64303aa33cfca32ef3bdfa694f4c06
Andreas <andreas.haukeland.95@gmail.com>
2017-11-02 13:05:41 +0100

    Add sensob

--- Sensob.py
+++ Sensob.py
@@ -1,1 +1,12 @@
 class Sensob:
+
+    def __init__(self, name, sensors = [], value = 0):
+        self.name = name
+        self.sensors = sensors
+        self.value = value
+
+    def update(self):
+        #Temporary code for receiving values from sensors
+        for sensor in self.sensors:
+            tempValue = sensor.get_value()
+
