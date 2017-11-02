c620ef1c3192444a032c6d3e1b601f34d3de691d
Larsern <laberge@stud.ntnu.no>
2017-11-02 15:34:54 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Arbitrator.py
@@ -0,0 +1,26 @@
+
+## The motor object (motob) manifests an interface between a behavior and one or more motors (a.k.a. actuators)
+
+import BBCON
+
+class arbitrator():
+
+    def __init__(self):
+        self.behaviors = {"Approche": 0, "Back Off": 0}
+        self.BBCONsActive_behaviors = BBCON.Active_behaviors
+
+    def uppdate(self):
+        for behaveObj in self.BBCONsActive_behaviors:
+            self.behaviors[behaveObj.getname] += behaveObj.getValue
+
+    def chooseBest(self):
+        sortedBehaviors = sorted(self.behaviors.items(), key=operator.itemgetter(1))
+        name = sortedBehaviors[-1][0]
+        for behavior in self.BBCONsActive_behaviors:
+            if behavior.getName() == name:
+                return behavior.recomondation()
+
+
+
+
+
