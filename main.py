eb5be1a14f6721b4b62ce0dace697fef11de01ca
Andreas <andreas.haukeland.95@gmail.com>
2017-11-07 16:37:33 +0100

    Wait for press

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from BBCON import BBCON
 from Arbitrator import Arbitrator
 from Motob import Motob
 from Behaviors import Approach, DetectEdge, ApproachRed
+from lib.zumo_button import ZumoButton
 
 bbcon = BBCON()
 arbitrator = Arbitrator()
 motob = Motob()
 approach = Approach()
-
+button = ZumoButton
 
 bbcon.set_arbitrator(arbitrator)
 bbcon.set_motob(motob)
@@ -15,7 +16,7 @@ bbcon.add_behavior(approach)
 
 bbcon.add_active_behavior(approach)
 
-
+button.wait_for_press()
 while(True):
     bbcon.run_one_timestep()
 
