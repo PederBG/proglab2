02edc24798c9fd36b70c3b0c97f0a9c37aa56f68
Andreas <andreas.haukeland.95@gmail.com>
2017-11-07 16:15:27 +0100

    Update

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from BBCON import BBCON
 from Arbitrator import Arbitrator
 from Motob import Motob
-from Behaviors import Approach, BackOff
+from Behaviors import Approach, DetectEdge, ApproachRed
 
 bbcon = BBCON()
 arbitrator = Arbitrator()
 motob = Motob()
 approach = Approach()
-back_off = BackOff()
+
 
 bbcon.set_arbitrator(arbitrator)
 bbcon.set_motob(motob)
 bbcon.add_behavior(approach)
-bbcon.add_behavior(back_off)
+
 bbcon.add_active_behavior(approach)
-bbcon.add_active_behavior(back_off)
+
 
 while(True):
     bbcon.run_one_timestep()
