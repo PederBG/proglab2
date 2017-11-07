5c573dec6caf45955f37600ad67be4da25aa65e9
Andreas <andreas.haukeland.95@gmail.com>
2017-11-07 15:11:52 +0100

    Behavior

--- Behaviors.py
+++ Behaviors.py
@@ -1,11 +1,18 @@
 from abc import abstractmethod
+import operator
 
 def recommended(command, speed=0.25, duration=0.1):
         return [command, speed, duration]
 
 class Behavior():
-    def __init__(self, controller):
-        self.controller = controller
+    def __init__(self):
+        self.action_rec = None
+        self.priority_weight = None
+        self.sens_obs =[]
+
+    @abstractmethod
+    def update(self):
+        pass
 
     @abstractmethod
     def get_action_recs(self):
@@ -15,11 +22,23 @@ class Behavior():
     def get_priority_weight(self):
         pass
 
-class Approach(Behavior):
-    def __init__(controller):
-        super().__init__(controller)
+    @abstractmethod
+    def get_name(self):
+        pass
 
-    def get_action_recs(self):
+class Approach(Behavior):
+    def __init__(self, look_ahead):
+        super().__init__()
+        super.sens_obs.append(look_ahead)
+
+    def update(self):
+        for element in self.sens_obs:
+            element.update()
+
+    def get_name(self):
+        return "Approach"
+
+    def get_action_rec(self):
         return recommended('L')
 
     def get_priority_weight(self):
@@ -30,17 +49,22 @@ class Approach(Behavior):
             return dist / 100
 
 class BackOff(Behavior):
-    def __init__(controller):
-        super().__init__(controller)
+    def __init__(self, look_ahead, look_under):
+        super().__init__()
+        super.sens_obs.append(look_ahead, look_under)
 
-    def get_action_recs(self):
-        return recommended('B')
+    def update(self):
+        for obs in self.sens_obs:
+            obs.update()
+
+        self.action_rec = recommended('L')
+        self.priority_weight = 1
+
+    def get_action_rec(self):
+        return self.action_rec
 
     def get_priority_weight(self):
-        dist = self.controller.lookAhed.get_value()
-        if dist > 100:
-            return 0
-        else:
-            return 100-dist / 100
+        return self.priority_weight
 
-    
+    def get_name(self):
+        return "BackOff"
