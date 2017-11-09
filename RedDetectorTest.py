4a3569d25b75c706beab8f20822f650a72f889c9
Pederduel <pederduel@gmail.com>
2017-11-09 16:41:15 +0100

    fixfix

--- RedDetectorTest.py
+++ RedDetectorTest.py
@@ -15,6 +15,12 @@ class RedDetect():
         left = []
         right = []
         all = []
+        blueAll = []
+        greenAll = []
+        blueLeft = []
+        greenLeft = []
+        blueRight = []
+        greenRight = []
         """left = im.crop((0, 0, im.width // 2, im.height))
         right = im.crop((im.width // 2, 0, im.width, im.height))
 
@@ -23,24 +29,32 @@ class RedDetect():
         for x in range(0,im.width//2):
             for y in range(im.height):
                 left.append(pix[x, y][0])
-        leftTurn = sum(left) // len(left)
+                blueLeft.append(pix[x, y][2])
+                greenLeft.append(pix[x, y][1])
+        leftRed = sum(left) // len(left)
 
         for x in range(im.width//2, im.width):
             for y in range(im.height):
                 right.append(pix[x, y][0])
-        rightTurn = sum(right) // len(right)
+                greenRight.append(pix[x, y][1])
+                blueRight.append(pix[x, y][2])
+        rightRed = sum(right) // len(right)
 
         for x in range(im.width):
             for y in range(im.height):
                 all.append(pix[x, y][0])
+                greenAll.append(pix[x, y][1])
+                blueAll.append(pix[x, y][2])
         forward = sum(all) // len(all)
 
-        if leftTurn/rightTurn > 3:
-            self.value = ("L", leftTurn/255)
-        elif rightTurn/leftTurn > 3:
-            self.value = ("R", rightTurn/255)
-        elif forward > 50 and leftTurn/rightTurn < 3 and rightTurn/leftTurn < 3:
+        if leftRed >= 40 and greenLeft < 40 and blueLeft < 40:
+            self.value = ("L", leftRed/255)
+        elif rightRed >= 40 and greenRight < 40 and blueRight < 40:
+            self.value = ("R", rightRed/255)
+        elif forward >= 50 and greenAll < 50 and blueAll < 50:
             self.value = ("F", forward/255)
+        else:
+            self.value = ("F", 0.0)
 
     def update(self):
         self.turnVal()
